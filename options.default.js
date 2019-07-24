14d56623abd4c22a10308990635be5614f91620a
BugKun <490964186@qq.com>
2019-07-25 00:19:02 +0800

    add a default options

---
+++ options.default.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+module.exports = {
+    root: path.resolve('./record'),
+    ffmpegPath: path.resolve('./ffmpeg/ffmpeg.exe'),
+    fprobePath: path.resolve('./ffmpeg/ffprobe.exe'),
+    port: 8080,
+    netkey: '',
+    CORS: '',
+    SCKEY: ''
+}
